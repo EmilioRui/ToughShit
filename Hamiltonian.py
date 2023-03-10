Fixed Qutip Time calculation

Hamiltonian.py
@@ -217,9 +217,8 @@ def make_dispersive(H, fock_trunc, fzpfs=None, f0s=None, chi_prime=False,
     else:
         return np.array(f1s), np.array(chis), np.array(fzpfs), np.array(f0s)
     
-def qutip_diag(mode_freqs,junc_freq,f_zp,fock_trunc,n_modes):
-    H=black_box_hamiltonian(mode_freqs,junc_freq,f_zp,fock_trunc=fock_trunc)
-    evals, evecs = H.eigenstates(eigvals= len(mode_freqs))
+def qutip_diag(H,n_modes):
+    evals, evecs = H.eigenstates(eigvals=n_modes)
     return evals
 
 